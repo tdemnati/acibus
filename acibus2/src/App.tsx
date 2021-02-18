d1e681c9b4f919cacae5f0a46dc1616ac367e999
tdemnati <tdemnati@gmail.com>
2021-02-18 23:29:30 +0100

    more modules

--- acibus2/src/App.tsx
+++ acibus2/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {TokenAnnotator} from './Annotator';
 import MyContext from './Components/Provider';
 import {Provider} from './Components/Provider';
+import ToggleEditTag from './Components/ToggleEditTag';
+import RadioTag from './Components/RadioTag';
 
 const TEXT = `On Monday night , Mr. Fallon will have a co-host for the first time : The rapper Cardi B , who just released her first album, " Invasion of Privacy . "`
 
@@ -27,95 +29,58 @@ const Card = ({children}) => (
 
 class App extends React.Component<any, any> {
 
-  
   render() {
 
-
     return (
-      <div>
+    <div>
       <Provider>
-      <div style={{padding: 24, fontFamily: 'IBM Plex Sans'}}>
-        <h3 style={{marginTop: 0}}>ACIBUS</h3>
-        <a href="http://a-cibus.com">Acibus</a>
-        <p>A React component for interactively highlighting parts of text.</p>
-        <h4>Edit tags</h4>
-        <MyContext.Consumer>
-        {(context)=> (
-        <button onClick={context.toggleEditTag}>{context.state.isEditTag ? "Edit" : "Unedit"}</button>)}
-        </MyContext.Consumer>
-        
-
-        <div style={{display: 'flex', marginBottom: 24}}>          
-              <Card>
-                <h4>Object Relation - tags</h4>
-                
-
-                <MyContext.Consumer>
-                      {(context)=> (
-                <div>
-                  {context.state.radios1.map((tag, idx) => (
-                      
-                      <label key={idx}>
-                      
-                        <input
-                                type="radio"
-                                value={tag}
-                                onChange={context.onSelectTag}
-                                checked={context.state.tag===tag}/>
-                                {tag}
-                                {context.state.isEditTag ? "": <button value={tag} onClick={context.deleteItem}>Del</button>}
-                       
-                       
-                      </label>
-                  ))}
-
-                  <br/>
-                  {context.state.isEditTag ? "": <div>
-                  <input type='text' onChange={context.onInputChange}/>
-                  <button onClick={context.addTag}>Add</button>
-                  <button onClick={context.resetTag}>Reset</button>
-                  </div>}
-
-
-                </div> 
-                      )}
-                </MyContext.Consumer>
-              
-                  
-                  <h4>Input Text</h4>
-                    <MyContext.Consumer>
-                      {(context)=> (
-                      <TokenAnnotator
-                          style={{
-                            fontFamily: 'IBM Plex Sans',
-                            maxWidth: 500,
-                            lineHeight: 1.5,
-                          }}
-                          tokens={TEXT.split(' ')}
-                          value={context.state.value}
-                          onChange={context.onSelectText}
-                          getSpan={span => ({
-                            ...span,
-                            tag: context.state.tag,
-                            color: TAG_COLORS[context.state.tag],
-                          })}
-                        />
-                      )}
-                    </MyContext.Consumer>
-              </Card>
+        <div>
+          <h3>ACIBUS</h3>
+          <a href="http://a-cibus.com">Acibus</a>
+          <p>A React component for interactively highlighting parts of text.</p>
+          <h4>Edit tags</h4>
+          <ToggleEditTag/>
+          <Card>
+            <h4>Tags</h4>
+            <RadioTag/>
+          </Card>
         </div>
-        <MyContext.Consumer>
-                      {(context)=> (
-        <Card>
-          <h4>Current Value</h4>
-          <pre>{JSON.stringify(context.state.value, null, 2)}</pre>
-        </Card>
-        )}
-                    </MyContext.Consumer>
-      </div>
-
+        <div>
+          <Card>
+            <h4>Text</h4>
+            <MyContext.Consumer>
+              {(context)=> (
+              <TokenAnnotator
+                  style={{
+                    fontFamily: 'IBM Plex Sans',
+                    maxWidth: 500,
+                    lineHeight: 1.5,
+                  }}
+                  tokens={TEXT.split(' ')}
+                  value={context.state.value}
+                  onChange={context.onSelectText}
+                  getSpan={span => ({
+                    ...span,
+                    tag: context.state.tag,
+                    color: TAG_COLORS[context.state.tag],
+                  })}
+                />
+              )}
+            </MyContext.Consumer>
+          </Card>
+          <Card>
+            <MyContext.Consumer>  
+              {(context)=> (
+                <>
+                  <h4>Output</h4>
+                  <pre>{JSON.stringify([...context.state.value, {text: TEXT}], null, 2)}</pre>
+                </>
+              )}
+            </MyContext.Consumer>
+          </Card>
+        </div>
       </Provider>
-            </div>
+    </div>
     )
   }
 }
